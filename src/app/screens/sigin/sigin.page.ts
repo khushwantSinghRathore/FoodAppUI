95f3ebfc3ad76e39d55c7f40b625b032a098016d
Khushwant Singh Rathore <31767649+khushwantSinghRathore@users.noreply.github.com>
2024-01-30 17:24:12 +0530

    signin page

--- src/app/screens/sigin/sigin.page.ts
+++ src/app/screens/sigin/sigin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sigin',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sigin.page.scss'],
 })
 export class SiginPage implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  login() {
+    this.router.navigateByUrl('/home');
   }
 
+  google() {}
+
+  fb() {}
+
+  fp() {
+    this.router.navigateByUrl('/forgetpass');
+  }
+
+  signup() {
+    this.router.navigateByUrl('/sigup');
+  }
 }
