488bee467d2426073069eeb69cc30c373fb5e813
Khushwant Singh Rathore <31767649+khushwantSinghRathore@users.noreply.github.com>
2024-01-30 19:05:57 +0530

    forgetpass page

--- src/app/screens/forgetpass/forgetpass.page.ts
+++ src/app/screens/forgetpass/forgetpass.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgetpass',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgetpass.page.scss'],
 })
 export class ForgetpassPage implements OnInit {
+  issubmit = false;
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  login() {}
+
+  submit() {
+    this.issubmit = true;
   }
 
+  resubmit() {
+    this.issubmit = false;
+  }
+
+  continue() {
+    this.router.navigateByUrl('/resetpass');
+  }
 }
