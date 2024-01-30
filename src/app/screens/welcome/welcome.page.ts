6e142dfe5d7c2b6a6422e709ea38cc59d27403ca
Khushwant Singh Rathore <31767649+khushwantSinghRathore@users.noreply.github.com>
2024-01-30 15:50:24 +0530

    welcome screen

--- src/app/screens/welcome/welcome.page.ts
+++ src/app/screens/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.page.scss'],
 })
 export class WelcomePage implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  signup() {
+    this.router.navigateByUrl('/sigup');
   }
 
+  signin() {
+    this.router.navigateByUrl('sigin');
+  }
 }
