dd29acaf8f5c146bca2c86f4526cd2a6124154c1
Khushwant Singh Rathore <31767649+khushwantSinghRathore@users.noreply.github.com>
2024-02-02 21:59:08 +0530

    Favorites Screen

--- src/app/favorites/favorites.page.ts
+++ src/app/favorites/favorites.page.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorites.page.scss'],
 })
 export class FavoritesPage implements OnInit {
+  itemoffer = [
+    {
+      img: 'assets/top-view-delicious-pasta-arrangement 1.png',
+      titl: 'Delicious Pasta',
+      dis: '$8.99 Delivery Fee . 15-30 min',
+      offr: 'Free Item(Spend $10)',
+      str: '4.6',
+    },
+    {
+      img: 'assets/delicious-cupcakes-with-colorful-icing 1.png',
+      titl: 'Delicious Cupcakes',
+      dis: '$5.99 Delivery Fee . 15-30 min',
+      offr: 'Buy 1 Get 1 Free',
+      str: '4.2',
+    },
+    {
+      img: 'assets/view-3d-delicious-looking-burger 1.png',
+      titl: 'Supreme Tacos',
+      dis: '$2.99 Delivery Fee . 15-30 min',
+      offr: 'Buy 1 Get 1 Free',
+      str: '4.6',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
